91077a48e05878cf5144841854d4ced59b07667d
skywa04885 <luke.rieff@fannst.nl>
2020-07-15 16:24:08 +0200

    I hate NodeJS way to ducking easy, making own webserver FSMTP

--- api/apps/fsmtp/src/services/Meta.service.tsx
+++
@@ -1,7 +0,0 @@
-export default class Meta
-{
-  public static setTitle(title: string): void
-  {
-    document.title = `${title} - Fannst Webmail`;
-  }
-}
--- api/src/options.ts
+++
@@ -1,5 +0,0 @@
-export const cassandraClientOptions = {
-  contactPoints: ['192.168.188.130'],
-  localDataCenter: 'Cassandra',
-  keyspace: 'fannst'
-};
