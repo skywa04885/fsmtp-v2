215db52e919a4eb7843568c409acc6c896b346d8
Luke Rieff <lrieff@fannst.nl>
2020-09-25 18:42:17 +0200

    Fixed header issue

--- lib/parsers/mimev2.src.cc
+++ lib/parsers/mimev2.src.cc
@@ -428,6 +428,7 @@ namespace FSMTP::Parsers {
     #ifdef _SMTP_DEBUG
     Logger logger("MIMEV2", LoggerLevel::DEBUG);
     Timer timer("parseMIME()", logger);
+    logger << "Parsing message: '" << ENDL << raw << ENDL << "'" << ENDL;
     #endif
 
     // Splits the message into lines, which later can be used
