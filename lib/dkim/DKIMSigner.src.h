b0c88da7c66d853c5f438bec2ff76767ebd6a846
Luke Rieff <webmaster@fannst.nl>
2020-10-01 15:45:43 +0200

    Fixed esmtp port

---
+++ lib/dkim/DKIMSigner.src.h
@@ -0,0 +1,45 @@
+/*
+	Copyright [2020] [Luke A.C.A. Rieff]
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+#ifndef _LIB_DKIM_SIGNER_H
+#define _LIB_DKIM_SIGNER_H
+
+namespace FSMTP::DKIM {
+	enum DKIMSignerAlgoPair {
+		DKIMSignerRelaxedRelaxed,
+		DKIMSignerRelaxedSimple,
+		DKIMSignerSimpleRelaxed,
+		DKIMSignerSimpleSimple
+	};
+
+	enum DKIMSignerSignAlgo {
+		DKIMSignerRSA_SHA1,
+		DKIMSignerRSA_SHA256
+	};
+
+	struct DKIMSignerConfig {
+		const char *domain;
+		const char *keySelector;
+		size_t signTime, expireTime;
+	};
+
+	class DKIMSigner {
+	public:
+	private:
+	};
+}
+
+#endif
